0280e738a8da8a64c46f47ce1a3b0f2b63e5abfb
Tarun Agrawal <agrawaljitarun@gmail.com>
2026-03-07 21:35:53 +0530

    Delete run-check.js
    Signed-off-by: Tarun Agrawal <agrawaljitarun@gmail.com>

--- run-check.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-
-exec("npx.cmd tsc --noEmit", (error, stdout, stderr) => {
-  fs.writeFileSync("tsc_output.txt", stdout + "\n" + stderr);
-  console.log("done");
-  process.exit(0);
-});
